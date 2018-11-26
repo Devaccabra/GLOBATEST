058f05c10d59f38848c489a8782f32fed172712d
William Ben Soussan <bensousan.william@gmail.com>
2018-11-26 21:57:04 +0100

    Create paravent.php

---
+++ paravent.php
@@ -0,0 +1,32 @@
+<?php
+$alt_vent = 0;
+$safe_zone = 0;
+
+do{
+    $continent = readline("Largeur du continent (comprise entre 1 et 100 000) : ");
+    if ($continent < 1 || $continent > 100000){
+        echo "Erreur : la valeure doit être comprise entre 1 et 100 000."."\r\n";
+    }
+}while ($continent < 1 || $continent > 100000);
+
+do{
+    $str_altitutes = readline("Entrer les ".$continent." valeurs séparées d'un espace (entre 0 et 100 000 : ");
+    $altitudes = explode(" ", $str_altitutes);
+}while(count($altitudes) != $continent);
+
+foreach ($altitudes as $altitude){
+    if ($altitude < 0 || $altitude > 100000){
+        echo "Erreur : Vous devez entrer des valeurs comprise entre 0 et 100 000";
+        die();
+    }
+}
+
+foreach ($altitudes as $altitude){
+    if ($altitude >= $alt_vent){
+        $alt_vent = $altitude;
+    }else{
+        $safe_zone++;
+    }
+}
+
+echo "Nombre de safe zone : ".$safe_zone;
