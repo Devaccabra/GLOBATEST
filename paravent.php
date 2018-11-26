d2036bbccac865063c2c8c205e21b05e5bfca5c9
William Ben Soussan <bensousan.william@gmail.com>
2018-11-26 21:59:20 +0100

    Update paravent.php

--- paravent.php
+++ paravent.php
@@ -3,16 +3,16 @@ $alt_vent = 0;
 $safe_zone = 0;
 
 do{
-    $continent = readline("Largeur du continent (comprise entre 1 et 100 000) : ");
-    if ($continent < 1 || $continent > 100000){
+    $nb_continent = readline("Largeur du continent (comprise entre 1 et 100 000) : ");
+    if ($nb_continent < 1 || $nb_continent > 100000){
         echo "Erreur : la valeure doit être comprise entre 1 et 100 000."."\r\n";
     }
-}while ($continent < 1 || $continent > 100000);
+}while ($nb_continent < 1 || $nb_continent > 100000);
 
 do{
-    $str_altitutes = readline("Entrer les ".$continent." valeurs séparées d'un espace (entre 0 et 100 000 : ");
+    $str_altitutes = readline("Entrer les ".$nb_continent." valeurs séparées d'un espace (entre 0 et 100 000 : ");
     $altitudes = explode(" ", $str_altitutes);
-}while(count($altitudes) != $continent);
+}while(count($altitudes) != $nb_continent);
 
 foreach ($altitudes as $altitude){
     if ($altitude < 0 || $altitude > 100000){
